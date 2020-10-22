9cadfb22fe9df9a0d55241936760f537b2777ac7
James Mernin <jamespmernin@gmail.com>
2020-10-22 16:33:01 -0400

    Solved algorithm

--- largestOfEach.js
+++ largestOfEach.js
@@ -1,7 +1,9 @@
 //LARGEST OF EACH ARRAY
+// Inspired by https://stackoverflow.com/questions/1669190/find-the-min-max-element-of-an-array-in-javascript
+// The above link helped me with getting the maximum of each array, but the map function was my own creation
 
 const largestOfEach = arr => {
-
+  return arr.map((e, i) => Math.max(...arr[i]));
 };
 
 console.log(
